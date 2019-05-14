dc79855c79782055c85caeeab676e7b441eb16de
Jono <jono@jyoung.co>
2019-05-15 09:58:10 +1000

    Updating dependency vulnerability
    Hopefully this fixes the vulnerability in the 'tar' dependency.

---
+++ nodejs-assets/build-native-modules-MacOS-helper-script-npm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+      # Helper script for Gradle to call npm on macOS in case it is not found
+      export PATH=$PATH:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/jono/Documents/GitHub/FritzCalls/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/jono/Documents/GitHub/FritzCalls/node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands
+      npm $@
+    
